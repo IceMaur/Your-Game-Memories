af71396eadb4c64d3ff01ea9907dfc0326f58f4d
Maurice Tulp <m.tulp@wewo.nl>
2024-12-09 15:58:32 +0100

    Base game details - Game cards navigate to game details - Game details page with background cover image

--- your-game-memories/frontend/src/App.jsx
+++ your-game-memories/frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/header/Header.jsx'
 import Home  from './pages/home/Home.jsx'
+import GameDetails from './pages/game-details/GameDetails.jsx';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/games/:id" element={<GameDetails/>}/>
       </Routes>
     </>
   )
---
+++ your-game-memories/frontend/src/pages/game-details/GameDetails.jsx
@@ -0,0 +1,41 @@
+import './GameDetails.css'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+function GameDetails(props) {
+  const [game, setGame] = useState(null);
+  const [error, setError] = useState(null);
+  let { id } = useParams();
+
+  useEffect(() => {
+    async function fetchGameDetails() {
+      try {
+        const result = await axios.get(`https://api.rawg.io/api/games/${id}?key=${import.meta.env.VITE_API_KEY_GAMES}`);
+        setGame(result.data);
+      } catch (e) {
+        setError(e);
+        console.error(e);
+      }
+    }
+
+    fetchGameDetails();
+  }, []); 
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  if (!game) {
+    return <p>Loading...</p>;
+  }
+
+
+  return (
+    <div id="game-details">
+      <img className="game-background-cover" src={game.background_image } />
+    </div>
+  )
+}
+  
+export default GameDetails
