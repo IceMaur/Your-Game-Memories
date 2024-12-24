d8ebdd23f06d920c19d2a79ffa26517bff6432d9
Maurice Tulp <m.tulp@wewo.nl>
2024-12-24 15:46:10 +0100

    Login form - UI - Login functionality

--- your-game-memories/frontend/src/App.jsx
+++ your-game-memories/frontend/src/App.jsx
@@ -3,14 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/header/Header.jsx'
 import Home  from './pages/home/Home.jsx'
 import GameDetails from './pages/game-details/GameDetails.jsx';
+import Login from './pages/login/login.jsx';
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/games/:id" element={<GameDetails/>}/>
+        <Route path="/" element={<Home />}/>
+        <Route path="/games/:id" element={<GameDetails />}/>
+        <Route path="/login" element={<Login />}/>
       </Routes>
     </>
   )
--- your-game-memories/frontend/src/pages/login/Login.jsx
+++ your-game-memories/frontend/src/pages/login/Login.jsx
@@ -1,9 +1,37 @@
 import './login.css'
+import axios from 'axios';
 
 function Login() {
+  const headers = {
+    'Content-Type': 'application/json',
+    'X-Api-Key': import.meta.env.VITE_API_KEY_BACKEND
+  }
+
+  async function handleLogin(e) {
+    e.preventDefault()
+    const username = e.target.username.value
+    const password = e.target.password.value
+
+    try {
+      const result = await axios.post('https://api.datavortex.nl/yourgamememories/users/authenticate', { username, password }, { headers: headers })
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
   return (
-    <>
-    </>
+    <div className='login'>
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" name="username" required />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" required />
+        <div className='login-buttons'>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
   )
 }
   
