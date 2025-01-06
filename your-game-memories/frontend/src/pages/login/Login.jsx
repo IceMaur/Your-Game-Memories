c6b2697541d938d59f1831c2a1d2934f8a575b63
Maurice Tulp <m.tulp@wewo.nl>
2025-01-06 08:05:50 +0100

    Login button disabeld

--- your-game-memories/frontend/src/pages/login/Login.jsx
+++ your-game-memories/frontend/src/pages/login/Login.jsx
@@ -1,4 +1,5 @@
 import './login.css'
+import React, { useState } from 'react';
 import axios from 'axios';
 
 function Login() {
@@ -7,13 +8,17 @@ function Login() {
     'X-Api-Key': import.meta.env.VITE_API_KEY_BACKEND
   }
 
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleChangeUsername = (e) => setUsername(e.target.value);
+  const handleChangePassword = (e) => setPassword(e.target.value);
+
   async function handleLogin(e) {
-    e.preventDefault()
-    const username = e.target.username.value
-    const password = e.target.password.value
+    e.preventDefault();
 
     try {
       const result = await axios.post('https://api.datavortex.nl/yourgamememories/users/authenticate', { username, password }, { headers: headers })
+      console.log(result);
     } catch (e) {
       console.error(e)
     }
@@ -24,11 +29,11 @@ function Login() {
       <h1>Login</h1>
       <form onSubmit={handleLogin}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" required />
+        <input type="text" id="username" name="username" value={username} onChange={handleChangeUsername} required />
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" required />
+        <input type="password" id="password" name="password" value={password} onChange={handleChangePassword} required />
         <div className='login-buttons'>
-          <button type="submit">Login</button>
+          <button type="submit" disabled={!username || !password}>Login</button>
         </div>
       </form>
     </div>
