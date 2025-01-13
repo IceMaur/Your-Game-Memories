edee4a78baefe486e3ae563b3042829931e9f0b2
Maurice Tulp <m.tulp@wewo.nl>
2025-01-13 15:09:00 +0100

    Searchbar

--- your-game-memories/frontend/src/pages/Home/Home.jsx
+++ your-game-memories/frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,17 @@
 import AllGamesOverview from "../../components/all-games-overview/AllGamesOverview";
 import './Home.css'
+import React, { useState } from 'react';
 
 function Home() {
+    const [searchText, setSearchText] = useState("");
+    const handleChangeSearchText = (e) => setSearchText(e.target.value);
+
   return (
     <div id="home-page">
-      <AllGamesOverview />
+      <div className="search-bar">
+        <input placeholder="Search..." className="search-bar-input" type="text" value={searchText} onChange={handleChangeSearchText} />
+      </div>
+      <AllGamesOverview searchText={searchText} />
     </div>
   )
 }
